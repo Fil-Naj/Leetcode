097a694f6a625ace5044e7e1a78b55beccba016f
Filip Najdovski <filipn1@optusnet.com.au>
2023-01-02 22:53:25 +0800

    Leetcode 2287: Rearrange Characters to Make Target String Time: 89.13% Memory: 13.4%

---
+++ Leetcode/Solutions/Leetcode2287.cs
@@ -0,0 +1,37 @@
+﻿using Leetcode.Extensions;
+
+namespace Leetcode.Solutions
+{
+    public class Leetcode2287 : ISolution
+    {
+        public string Name => "Rearrange Characters to Make Target String";
+        public string Description => "You are given two 0-indexed strings s and target. You can take some letters from s and rearrange them to form new strings.\r\n\r\nReturn the maximum number of copies of target that can be formed by taking letters from s and rearranging them.";
+        public Difficulty Difficulty => Difficulty.Easy;
+
+        public void Solve()
+        {
+            this.PrintProblemStatement();
+
+            var s = "ilovecodingonleetcode";
+            var target = "code";
+            var result = RearrangeCharacters(s, target);
+
+            // Prettify
+            Console.WriteLine($"Input: s = {s}, target = {target}");
+            Console.WriteLine($"Output: {result}");
+        }
+
+        public int RearrangeCharacters(string s, string target)
+        {
+            Dictionary<char, int> sf = s.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            Dictionary<char, int> tf = target.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+
+            var result = int.MaxValue;
+            foreach (var c in tf)
+            {
+                result = Math.Min(result, sf.TryGetValue(c.Key, out var value) ? value / c.Value : 0);
+            }
+            return result;
+        }
+    }
+}
