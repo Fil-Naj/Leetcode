89283b11f6f562742ad18a223799a5a1547e96de
Filip Najdovski <filip.najdovski@outlook.com>
2023-02-13 09:44:43 +0800

    Leetcode 1523 : Count Odd Numbers in an Interval Range Time: 84.77% Memory: 79.65%

---
+++ Leetcode/Solutions/Leetcode1523.cs
@@ -0,0 +1,29 @@
+﻿using Leetcode.Extensions;
+
+namespace Leetcode.Solutions
+{
+    public class Leetcode1523 : ISolution
+    {
+        public string Name => "Count Odd Numbers in an Interval Range";
+        public string Description => "Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).";
+        public Difficulty Difficulty => Difficulty.Easy;
+
+        public void Solve()
+        {
+            this.PrintProblemStatement();
+
+            var low = 7;
+            var high = 10;
+            var result = CountOdds(low, high);
+
+            // Prettify
+            Console.WriteLine($"Input: low = {low}, high = {high}");
+            Console.WriteLine($"Output: {result}");
+        }
+
+        public int CountOdds(int low, int high)
+        {
+            return (int)Math.Ceiling(((decimal)high - (decimal)low) / 2m) + (low % 2 == 1 && high % 2 == 1 ? 1 : 0);
+        }
+    }
+}
