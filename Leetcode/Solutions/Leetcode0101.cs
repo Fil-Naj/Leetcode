016fb53941db0edd9db31947ecfbc75688aed7b3
Filip-Naj <filipn1@optusnet.com.au>
2023-03-13 09:42:41 +0800

    Leetcode 101 : Symmetric Tree Time: 66.50% Memory: 66.19%

---
+++ Leetcode/Solutions/Leetcode0101.cs
@@ -0,0 +1,46 @@
+﻿using Leetcode.Common_Objects;
+using Leetcode.Extensions;
+
+namespace Leetcode.Solutions
+{
+    public class Leetcode0101 : ISolution
+    {
+        public string Name => "Symmetric Tree";
+        public string Description => "Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).";
+        public Difficulty Difficulty => Difficulty.Easy;
+
+        public void Solve()
+        {
+            this.PrintProblemStatement();
+
+            TreeNode root = new(2,
+                new(3,
+                    new(4),
+                    new(5)),
+                new(3,
+                    null,
+                    new(4))
+            );
+            var result = IsSymmetric(root);
+
+            // Prettify
+            root.PrintBinaryTree();
+            Console.WriteLine($"Output: {result}");
+        }
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+
+            bool IsSymmetricRecursive(TreeNode left, TreeNode right)
+            {
+                if (left == null && right == null) return true;
+                if (left?.val != right?.val) return false;
+
+                return IsSymmetricRecursive(left.left, right.right) && IsSymmetricRecursive(left.right, right.left);
+            }
+
+            return IsSymmetricRecursive(root.left, root.right);
+        }
+    }
+}
