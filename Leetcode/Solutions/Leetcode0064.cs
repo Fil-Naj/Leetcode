cb04b8c60b7adfbc2d5013f682c4f84a77c74af4
Filip Najdovski <filip.najdovski@outlook.com>
2023-03-27 10:27:53 +0800

    Leetcode 64 : Minimum Path Sum (dp approach) Time: 95.77% Memory: 92.9%

--- Leetcode/Solutions/Leetcode0064.cs
+++ Leetcode/Solutions/Leetcode0064.cs
@@ -28,35 +28,56 @@ namespace Leetcode.Solutions
             Console.WriteLine($"Output: {result}");
         }
 
+        //public int MinPathSum(int[][] grid)
+        //{
+        //    int m = grid.Length;
+        //    int n = grid[0].Length;
+
+        //    PriorityQueue<(int x, int y), int> queue = new();
+        //    queue.Enqueue((0, 0), grid[0][0]);
+        //    HashSet<(int x, int y)> visited = new() { (0, 0) };
+
+        //    while (queue.TryDequeue(out var node, out var sum))
+        //    {
+        //        if (node.x == n - 1 && node.y == m - 1) return sum;
+
+        //        // Try go right
+        //        if (node.x < n - 1 && !visited.Contains((node.x + 1, node.y)))
+        //        {
+        //            queue.Enqueue((node.x + 1, node.y), sum + grid[node.y][node.x + 1]);
+        //            visited.Add((node.x + 1, node.y));
+        //        }
+
+        //        // Try go down
+        //        if (node.y < m - 1 && !visited.Contains((node.x, node.y + 1)))
+        //        {
+        //            queue.Enqueue((node.x, node.y + 1), sum + grid[node.y + 1][node.x]);
+        //            visited.Add((node.x, node.y + 1));
+        //        }
+        //    }
+
+        //    return -1;
+        //}
+
+        // Dynamic Programmin approach
         public int MinPathSum(int[][] grid)
         {
-            int m = grid.Length;
-            int n = grid[0].Length;
-
-            PriorityQueue<(int x, int y), int> queue = new();
-            queue.Enqueue((0, 0), grid[0][0]);
-            HashSet<(int x, int y)> visited = new() { (0, 0) };
-
-            while (queue.TryDequeue(out var node, out var sum))
+            for (int row = 0; row < grid.Length; row++)
             {
-                if (node.x == n - 1 && node.y == m - 1) return sum;
-
-                // Try go right
-                if (node.x < n - 1 && !visited.Contains((node.x + 1, node.y)))
+                for (int col = 0; col < grid[row].Length; col++)
                 {
-                    queue.Enqueue((node.x + 1, node.y), sum + grid[node.y][node.x + 1]);
-                    visited.Add((node.x + 1, node.y));
-                }
+                    if (col == 0 && row == 0) continue;
 
-                // Try go down
-                if (node.y < m - 1 && !visited.Contains((node.x, node.y + 1)))
-                {
-                    queue.Enqueue((node.x, node.y + 1), sum + grid[node.y + 1][node.x]);
-                    visited.Add((node.x, node.y + 1));
+                    if (col == 0) 
+                        grid[row][col] = grid[row - 1][col] + grid[row][col];
+                    else if (row == 0) 
+                        grid[row][col] = grid[row][col - 1] + grid[row][col];
+                    else 
+                        grid[row][col] = Math.Min(grid[row][col - 1], grid[row - 1][col]) + grid[row][col];
                 }
             }
 
-            return -1;
+            return grid[^1][grid[0].Length - 1];
         }
     }
 }
