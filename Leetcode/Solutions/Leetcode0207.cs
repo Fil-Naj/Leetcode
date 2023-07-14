b418a31e2e5a872789c59a634a69fcb46577a2ce
Filip-Naj <filipn1@optusnet.com.au>
2023-07-14 10:40:30 +0800

    Leetcode 1218 : Longest Arithmetic Subsequence of Given Difference Time: 53.57% Memory: 92.86%

--- Leetcode/Solutions/Leetcode0207.cs
+++ Leetcode/Solutions/Leetcode0207.cs
@@ -4,7 +4,7 @@ namespace Leetcode.Solutions
 {
     public class Leetcode0207 : ISolution
     {
-        public string Name => " Course Schedule";
+        public string Name => "Course Schedule";
         public string Description => "There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.\r\n\r\n    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.\r\n\r\nReturn true if you can finish all courses. Otherwise, return false.";
         public Difficulty Difficulty => Difficulty.Medium;
 
@@ -45,8 +45,7 @@ namespace Leetcode.Solutions
                 if (!visited.Contains(i))
                 {
                     visited.Add(i);
-                    var prereqs = adj.TryGetValue(i, out var pre) ? pre : new();
-                    if (prereqs.All(IsPossibleCourse))
+                    if (!adj.TryGetValue(i, out var pre) || pre.All(IsPossibleCourse))
                         possibleCourses.Add(i);
                 }
 
