21192fff436c1866e01b62414c4ee6e7ac171a02
Filip Najdovski <filip.najdovski@outlook.com>
2024-02-05 11:28:31 +0800

    Leetcode 14 : Longest Common Prefix Time: 79.95% Memory: 76.25%

---
+++ Leetcode/Solutions/Leetcode0014.cs
@@ -0,0 +1,39 @@
+﻿using Leetcode.Extensions;
+using System.Text;
+
+namespace Leetcode.Solutions
+{
+    public class Leetcode0014 : ISolution
+    {
+        public string Name => "Longest Common Prefix";
+        public string Description => "Write a function to find the longest common prefix string amongst an array of strings.\r\n\r\nIf there is no common prefix, return an empty string \"\".";
+        public Difficulty Difficulty => Difficulty.Easy;
+
+        public void Solve()
+        {
+            this.PrintProblemStatement();
+
+            string[] data = ["flower", "flow", "flight"];
+            var result = LongestCommonPrefix(data);
+
+            // Prettify
+            Console.WriteLine($"Input: data = {data.GetString()}");
+            Console.WriteLine($"Output: {result}");
+        }
+
+        public string LongestCommonPrefix(string[] strs)
+        {
+            StringBuilder sb = new();
+            var minLength = strs.Min(s => s.Length);
+
+            var i = 0;
+            while (i < minLength && strs.All(s => s[i] == strs[0][i]))
+            {
+                sb.Append(strs[0][i]);
+                i++;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
