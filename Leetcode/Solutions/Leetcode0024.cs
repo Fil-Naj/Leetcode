3592c9f98d7874b6f04458d1f32ec6388d9f90c8
Fil-Naj <filip.najdovski@outlook.com>
2024-09-11 11:12:28 +0800

    Leetcode 24 : Swap Nodes in Pairs Time: 81.68% Memory: 74.42%

---
+++ Leetcode/Solutions/Leetcode0024.cs
@@ -0,0 +1,48 @@
+﻿using Leetcode.Common_Objects;
+using Leetcode.Extensions;
+
+namespace Leetcode.Solutions
+{
+    public class Leetcode0024 : ISolution
+    {
+        public string Name => "Swap Nodes in Pairs";
+        public string Description => "Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)";
+        public Difficulty Difficulty => Difficulty.Medium;
+
+        public void Solve()
+        {
+            this.PrintProblemStatement();
+
+            var head = new ListNode([1, 2, 3, 4, 5]);
+
+            // Prettify
+            Console.WriteLine($"Input: head = {head}");
+            var result = SwapPairs(head);
+            Console.WriteLine($"Output: {result}");
+        }
+
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head is null || head.next is null) return head;
+
+            var toReturn = new ListNode(0, head.next);
+            var prev = toReturn;
+            var current = head;
+            var adj = current.next;
+
+            while (current is not null && adj is not null)
+            {
+                var tempNext = adj.next;
+                (current.next, adj.next) = (adj.next, current);
+
+                prev.next = adj;
+                prev = current;
+
+                current = tempNext;
+                adj = current?.next;
+            }
+
+            return toReturn.next;
+        }
+    }
+}
